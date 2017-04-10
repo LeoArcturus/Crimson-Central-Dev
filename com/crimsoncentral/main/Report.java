c3df6ea6e807bc20d08c0f6f3ff6cc58b07bcf05
MrGamingLion <mrgaminglion50@gmail.com>
2017-04-10 17:01:55 -0600

    Create Report.java

---
+++ com/crimsoncentral/main/Report.java
@@ -0,0 +1,36 @@
+package com.crimsoncentral.main.BanReport;
+
+public class Report
+{
+    public Player player;
+    public List<String> discriptions = new ArrayList<String>();
+    pulbic List<Player> accusers = new ArrayList<Player>();
+    
+    public Report(Player p, String message)
+    {
+         this.player = p
+         this.discriptions.add(message);
+         this.accusers.add(p);
+    }
+    
+    public void addAccusation(Player p, String message)
+    {
+        this.discriptions.add(message);
+        this.accusers.add(p);
+    }
+    
+    public Player getPlayer()
+    {
+        return this.player;
+    }
+    
+    public ArrayList<Player> getAccusers()
+    {
+        return this.accusers;
+    }
+    
+    public ArrayList<String> getAccusations()
+    {
+        return this.discriptions;
+    }
+}
