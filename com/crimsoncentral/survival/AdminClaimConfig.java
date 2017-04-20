85d66728e0c9db9381bfe1b60c8c56e5ae4dd8a4
MrGamingLion <mrgaminglion50@gmail.com>
2017-04-20 14:58:29 -0600

    Create AdminClaimConfig.java

---
+++ com/crimsoncentral/survival/AdminClaimConfig.java
@@ -0,0 +1,55 @@
+package com.crimsoncentral.survival;
+
+public class AdminClaimConfim
+{
+    public File plotConfig;
+    public FileConfiguration plotData;
+    
+    public AdminClaimConfim()
+    {      
+        plotConfig = new File("plugins/CrimsonCentralv1/Survival/PlotClaim/Server_Claim.yml");
+        plotData = YamlConfiguration.loadConfiguration(plotConfig);
+    }
+    
+    public World getPlotWorld()
+    {
+        String s = plotData.getString("World");
+        return Bukkit.getWorld(s);
+    }
+    
+    public void setPlotWorld(World w)
+    {
+        plotData.set("World", w.getName());
+    }
+    
+    public Location getPos1(int i)
+    {
+        String location = plotData.getInt("Claim " + i + ".Pos 1");
+        Location loc;
+	      StringTokenizer st = new StringTokenizer(s);
+	      loc.setX(st.nextToken());
+	      loc.setY(st.nextToken());
+	      loc.setZ(st.nextToken());
+	      return loc;
+    }
+    
+    public void setPos1(int i, Location loc)
+    {
+        plotData.set("Claim" + i + ".Pos 1", loc.getX() + ", " + loc.getY() + ", " + loc.getZ())
+    }
+    
+    public Location getPos2(int i)
+    {
+        return plotData.getInt("Claim " + i + ".Pos 2");
+        StringTokenizer st = new StringTokenizer(s);
+	      loc.setX(st.nextToken());
+	      loc.setY(st.nextToken());
+	      loc.setZ(st.nextToken());
+	      return loc;
+    }
+    
+    public void setPos2(int i, Location loc)
+    {
+        plotData.set("Claim" + i + ".Pos 2", loc.getX() + ", " + loc.getY() + ", " + loc.getZ())
+    }
+}
