82f5fb0b375365f53e4ea38a5446eb321a0efd87
MrGamingLion <mrgaminglion50@gmail.com>
2017-04-20 15:05:07 -0600

    Create PlotClaimCommands.java

---
+++ com/crimsoncentral/survival/PlotClaimCommands.java
@@ -0,0 +1,12 @@
+package com.crimsoncentral.survival;
+
+public class PlotClaimCommands implements CommandExecutor
+{
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase("plotclaim") || cmd.getName().equalsIgnoreCase("pc") cmd.getName().equalsIgnoreCase("sp")) 
+        {
+
+        }
+    }
+}
